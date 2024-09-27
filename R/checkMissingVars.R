1439eb82860a2eb95e2cceed2e6c6ff8bf892cdc
orichters <oliverr@pik-potsdam.de>
2024-09-27 18:44:39 +0200

    buildLibrary

--- R/checkMissingVars.R
+++ R/checkMissingVars.R
@@ -3,7 +3,10 @@
 #' @md
 #' @author Oliver Richters
 #' @param mifdata data that can be read with as.quitte
-#' @inheritParams getMappingVariables
+#' @param mapping name of the project of requested mappings
+#'        such as c('AR6', 'AR6_NGFS') or 'mapping.csv'. Use TRUE for all mappings.
+#' @param sources model abbreviation(s) used in 'source' column.
+#'        R = REMIND, M = MAgPIE, T = EDGE-T, B = Brick, C = Climate/MAGICC, TRUE = all
 #' @importFrom piamutils deletePlus
 #' @importFrom quitte as.quitte
 #' @return an invisible vector with missing variables
@@ -14,8 +17,9 @@ checkMissingVars <- function(mifdata, mapping = TRUE, sources = TRUE) {
   computedVariables <- unique(paste0(deletePlus(mifdata$variable), " (", gsub("^$", "unitless", mifdata$unit), ")"))
   missingVariables <- sort(setdiff(mappingVariables, computedVariables))
   if (length(missingVariables) > 0) {
-    message("# The following ", length(missingVariables), " variables are expected in the piamInterfaces package ",
-            "for mapping ", paste(mapping, collapse = ", "), " and source ", sources,
+    message("# The following ", length(missingVariables), " variables are expected in the piamInterfaces package",
+            if (! isTRUE(mapping)) paste0(" for mapping ", paste(mapping, collapse = ", ")),
+            if (! isTRUE(sources)) paste0(" and source ", sources),
             ", but cannot be found in the reporting:\n- ", paste(missingVariables, collapse = ",\n- "), "\n")
   }
   return(invisible(missingVariables))
